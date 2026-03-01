daca8099b09b6c97153cba25ee87eeab32c8c032
Viditjn02 <viditjn02@gmail.com>
2026-03-01 05:07:50 -0800

    Remove unused ReactQueryDevtools import (breaks production build)

--- apps/game-frontend/src/app/providers.tsx
+++ apps/game-frontend/src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { ReactNode } from 'react';
 import { getQueryClient } from './get-query-client';
 
